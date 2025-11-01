8f93934c59c7a38c79a66d32a40dcef0660e0bdf
Gáspár Csanád <csanad0502@gmail.com>
2025-11-01 10:39:50 +0100

    Refactors UI with Tailwind CSS and Radix UI
    Migrates the frontend UI to use Tailwind CSS for styling and Radix UI
    components for improved accessibility and composability.
    
    Removes Mantine UI and related dependencies.
    
    Introduces new UI components (button, badge) based on Radix UI primitives.
    
    Adds tailwind-merge and class-variance-authority for managing Tailwind CSS classes.

--- Frontend/src/pages/Landing.tsx
+++ Frontend/src/pages/Landing.tsx
@@ -1,42 +1,39 @@
-import {BackgroundImage, Box, Center, Container, Image, LoadingOverlay, Paper, Text} from "@mantine/core";
-import {useState} from "react";
 
-
+//import {useState} from "react";
+import { Badge } from "@/components/ui/badge"
+import {Shield, Vote, CircleQuestionMark} from "lucide-react"
+import {Button} from "@/components/ui/button.tsx";
 function Landing(){
-
-    const [isLoading, setIsLoading] = useState(true);
 
 
     return(
-        <BackgroundImage src={`/orszaghaz.jpg`} style={{ backgroundImage: `url(/orszaghaz.jpg)`,
-            backgroundSize: "cover",
-            position: 'sticky',
-            backgroundPosition: "center",
-            minHeight: "100dvh",
-            width: "100vw",
-            height: "100dvh",
+        <div className="relative min-h-screen w-full bg-gradient-to-br from-gray-100 to-gray-100 bg-no-repeat bg-contain bg-center text-white">
+            <div className="container mx-auto px-4 sm:px-6 lg:px-8 relative">
+                <div className="grid lg:grid-cols-2 gap-12 items-center pt-10 "> {/* items-centered*/}
+                   <div className="space-y-2">
+                           <Badge variant="outline"  className= "text-black bg-gray-300">{<Shield className="h-4 w-4"/>} 100% Biztonságos és Átlátható</Badge>
+                           <div className="flex flex-col gap-2">
+                               <h3 className="scroll-m-20 text-2xl font-extrabold tracking-tight text-balance text-outline text-white">Magyar Országgyűlési választások </h3>
+                           </div>
+                           <p className="text-black mb-10">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+                       <div className="space-x-10 text-center">
+                           <Button className="bg-green-700">
+                               <Vote/> Szavazat leadása
+                           </Button>
+                           <Button >
+                               <CircleQuestionMark/>További információk
+                           </Button>
+                       </div>
+                   </div>
+                    <div className="relative">
+                        <div className=" overflow-hidden rounded-2xl shadow-2xl border-4 border-white">
+                            <img src="./orszaghaz.jpg" alt="Országház" />
+                        </div>
+                    </div>
+                </div>
+            </div>
 
-        }}>
-            <Box
-                style={{
-                    position: "absolute",
-                    inset: 0,
-                    backdropFilter: "blur(2px)",
-                    backgroundColor: "rgba(0,0,0,0.4)", // sötétebb árnyalat a kontraszthoz
-                    zIndex: 0,
-                }}
-            />
-
-            <Container pos='relative' fluid p={0} style={{zIndex:1,textAlign:"center"}}>
-                <LoadingOverlay visible={isLoading} overlayProps={{ blur: 2 }}/>
-                <Paper p="xl"
-                       radius="lg">
-                    <Center>
-                        <Text>dfsdfsdzd</Text>
-                    </Center>
-                </Paper>
-            </Container>
-        </BackgroundImage>
+        </div>
     )
 }
 
