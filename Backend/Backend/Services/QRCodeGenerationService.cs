f2b087876dcb4e6eb7af0ddddbd5666919650210
Gáspár Csanád <csanad0502@gmail.com>
2025-11-14 18:04:02 +0100

    Adds QR code generation service
    Implements a service for generating QR codes, including encryption for enhanced security.
    
    The service encrypts payloads for voting tokens, generates QR codes using the ZXing library, and creates PDF documents containing these QR codes using QuestPDF.
    
    Adds required dependencies for QR code generation and PDF creation.

--- Backend/Backend/Services/QRCodeGenerationService.cs
+++ Backend/Backend/Services/QRCodeGenerationService.cs
@@ -5,6 +5,12 @@ using AutoMapper;
 using Backend.Context;
 using Backend.DTOs;
 using Microsoft.EntityFrameworkCore;
+using QuestPDF.Fluent;
+using QuestPDF.Helpers;
+using QuestPDF.Infrastructure;
+using ZXing;
+using ZXing.QrCode;
+using ZXing.Rendering;
 
 namespace Backend.Services;
 
@@ -22,16 +28,73 @@ public class QRCodeGenerationService
     
     public async Task<string> GenerateQRCodesForDistrinct(string DistrictId)
     {
+        var subectDistrict = await _context.VotingDistricts.FirstOrDefaultAsync(c=>c.Id.ToString() == DistrictId) ?? throw new Exception("District not found");
+        var DistrictName = $"{subectDistrict.CountyName}_{subectDistrict.OEVK}_{subectDistrict.CityName}_{subectDistrict.PollingStationAddress}_StationNuber_{subectDistrict.PollingStationNumber}"
+        
         var votingDistricts = await _context.VotingTokens.Where(c=> c.VotingDistrictId.ToString() == DistrictId).ToListAsync() ?? throw new Exception("No districts found");
-        foreach (var district in votingDistricts)
+        
+        var payloadList = votingDistricts.Where(k => k.VotingDistrictId.ToString().Equals(DistrictId)).Select(c=> new DataPayload
         {
-            
-        }
+            Token = c.VotingToken.ToString(),
+            VotingDist = c.VotingDistrictId.ToString()
+        }).ToList();
 
+        var encryptedPayloads = payloadList.Select(EncryptPayload);
+
+        Console.WriteLine($"{encryptedPayloads.Count()} payloads generated successfully");
+        
+        QuestPDF.Settings.License = LicenseType.Community;
+
+        var document = Document.Create(container =>
+            container.Page(page =>
+            {
+                page.Size(PageSizes.A4);
+                page.Margin(1 , Unit.Centimetre);
+
+                page.Content().PaddingVertical(4).Table(table =>
+                {
+
+                    table.ColumnsDefinition(columns =>
+                    {
+                        columns.RelativeColumn();
+                        columns.RelativeColumn();
+                    });
+                    
+                    foreach (var encryptedString in encryptedPayloads)
+                    {
+                        table.Cell().Padding(3).Element(tablecell =>
+                        {
+                            tablecell
+                                .Border(1)
+                                .BorderColor(Colors.Grey.Medium)
+                                .Padding(3)
+                                .Column(column =>
+                                {
+                                    column.Item()
+                                        .Height(120)
+                                        .AlignCenter()
+                                        .AlignMiddle()
+                                        .Svg(size =>
+                                        {
+                                            var writer = new QRCodeWriter();
+                                            var qrCode = writer.encode(encryptedString, BarcodeFormat.QR_CODE, (int)size.Width, (int)size.Height);
+                                            var renderer = new SvgRenderer { FontName = "Lato" };
+                                            return renderer.Render(qrCode, BarcodeFormat.QR_CODE, null).Content;
+                                            //A Barcode itt roszsul volt ha hibás lenne 
+                                        });
+                                });
+                        });
+                    }
+                });
+            }));
+
+        document.GeneratePdf($"{DistrictName}");
         return ("fsd");
     }
 
-    public string EncryptPayload(DataPayload dataPayload)
+    
+    
+    private string EncryptPayload(DataPayload dataPayload)
     {
         var datapayload = JsonSerializer.Serialize(dataPayload);
         var plaintext = Encoding.UTF8.GetBytes(datapayload);
@@ -65,7 +128,40 @@ public class QRCodeGenerationService
 
         return Convert.ToBase64String(encryptedResoult);
     }
-    
-    
+
+    private DataPayload DecryptData(string encryptedData)
+    {
+        var encryptedString = Convert.FromBase64String(encryptedData);
+
+
+        var secretKey = _config.GetSection("SecretKey").Value!;
+        
+        const int keySize = 32;
+        const int saltSize = 16;
+        const int nonceSize = 12;
+        const int tagSize = 16;
+        const int iterations = 100000;
+
+
+        var salt = encryptedString[..saltSize];
+        var nonce = encryptedString[saltSize.. (saltSize + nonceSize)];
+        var tag = encryptedString[(saltSize + nonceSize).. (saltSize + nonceSize + tagSize)];
+        var ciphertext = encryptedString[(saltSize + nonceSize + tagSize)..];
+        
+        //Key regenerate
+
+        
+        var kdf = new Rfc2898DeriveBytes(secretKey, salt, iterations, HashAlgorithmName.SHA256);
+        var key = kdf.GetBytes(keySize);
+        
+        var plaintext = new byte[ciphertext.Length];
+
+        using var aesGcm = new AesGcm(key);
+        aesGcm.Decrypt(nonce,ciphertext,tag,plaintext);
+        
+        var payload = JsonSerializer.Deserialize<DataPayload>(plaintext) ?? throw new Exception("Failed to deserialize payload");
+
+        return payload;
+    }
     
 }
